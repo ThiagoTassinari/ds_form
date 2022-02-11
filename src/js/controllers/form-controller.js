1eb04d29dd371f179c7927e31524fd37e01a5a85
ThiagoTassinari <thiagotassinari12@gmail.com>
2022-02-11 00:23:32 -0300

    Selecting form elements

--- src/js/controllers/form-controller.js
+++ src/js/controllers/form-controller.js
@@ -1,8 +1,33 @@
 
+//Função construtora para representar qual o tipo do estado do módulo
+function State() {
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+    
+    this.errorCep = null;
+    this.errorNumber = null;
+ 
+    this.btnSave = null;
+    this.btnClear = null;
+}
 
+// Instaciei um objeto com nossa função construtora State()
+const state = new State();
 
+// Inicializa todos os valores do estado dentro da função init
 export function init() {
-    const inputCep = document.forms.newAddress.cep;
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
 
-    console.log(inputCep);
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
+
+    state.errorCep = document.querySelector('[data-error="cep"]');
+    state.errorNumber = document.querySelector('[data-error="number"]');
+
+    console.log(state);
 }
